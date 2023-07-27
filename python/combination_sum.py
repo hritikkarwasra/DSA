b9273eaa646c32c9b84ab7075dd5f8214f7e7c38
hritikkarwasra <khrithik0806@gmail.com>
2023-07-28 00:09:01 +0530

    solving recursion problems day3

---
+++ python/combination_sum.py
@@ -0,0 +1,46 @@
+# Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+# The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+# frequency
+#  of at least one of the chosen numbers is different.
+
+# The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+# Example 1:
+
+# Input: candidates = [2,3,6,7], target = 7
+# Output: [[2,2,3],[7]]
+# Explanation:
+# 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+# 7 is a candidate, and 7 = 7.
+# These are the only two combinations.
+# Example 2:
+
+# Input: candidates = [2,3,5], target = 8
+# Output: [[2,2,2,2],[2,3,3],[3,5]]
+# Example 3:
+
+# Input: candidates = [2], target = 1
+# Output: []
+
+class Solution:
+    def generator(self, i, ans, res, candidates, target):
+        if i >= len(candidates) or sum(res)> target: return
+
+        if sum(res) == target:
+            print(res)
+            return ans.append(res[:])
+        
+        res.append(candidates[i])
+        # print(res)
+        self.generator(i, ans, res, candidates, target)
+
+        res.pop()
+        self.generator(i+1, ans, res, candidates, target)
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        self.generator(0, ans, [], candidates, target)
+        return ans
